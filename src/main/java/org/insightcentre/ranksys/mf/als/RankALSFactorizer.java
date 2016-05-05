c486ab8642fdf338621a28b919768c1a56b73a51
Jacek Wasilewski <jacek.wasilewski@outlook.ie>
2016-05-05 19:50:44 +0100

    fixing license

--- src/main/java/org/insightcentre/ranksys/mf/als/RankALSFactorizer.java
+++ src/main/java/org/insightcentre/ranksys/mf/als/RankALSFactorizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 RankSys http://ranksys.org
+ * Copyright (C) 2016 Insight Centre for Data Analytics http://insight-centre.org
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
